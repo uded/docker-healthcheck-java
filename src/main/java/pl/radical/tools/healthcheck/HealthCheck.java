cb4f843206d6d27125b5abdaf28eb587f2408796
Łukasz Rżanek <lukasz.rzanek@radical.com.pl>
2018-11-03 22:05:20 +0200

    Zmiany dla lepszej obsługi Spring Actuator

--- src/main/java/pl/radical/tools/healthcheck/HealthCheck.java
+++ src/main/java/pl/radical/tools/healthcheck/HealthCheck.java
@@ -5,12 +5,32 @@ import com.goebl.david.WebbException;
 import lombok.val;
 import org.json.JSONException;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.jar.Attributes;
+import java.util.jar.Manifest;
+
 /**
  * @author <a href="mailto:lukasz.rzanek@radical.com.pl">Łukasz Rżanek</a>
  * @since 11.05.2018
  */
 public class HealthCheck {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        if (args == null || args.length == 0) {
+            Class clazz = HealthCheck.class;
+            String className = clazz.getSimpleName() + ".class";
+            String classPath = clazz.getResource(className).toString();
+            if (!classPath.startsWith("jar")) {
+                // Class not from JAR
+                System.exit(11);
+            }
+            String manifestPath = classPath.substring(0, classPath.lastIndexOf("!") + 1) + "/META-INF/MANIFEST.MF";
+            Manifest manifest = new Manifest(new URL(manifestPath).openStream());
+            Attributes attr = manifest.getMainAttributes();
+            String value = attr.getValue("Implementation-Version");
+            System.out.println("Version: " + value);
+            System.exit(0);
+        }
         if (checkStatus(args[0])) {
             System.exit(0); // IT WORKED
         } else {
@@ -21,15 +41,22 @@ public class HealthCheck {
     static boolean checkStatus(String url) {
         try {
             Webb webb = Webb.create();
-            val response = webb.get(url).ensureSuccess().asJsonObject().getBody();
-            if (response.has("status") && response.getString("status").equalsIgnoreCase("UP")) {
+            val response = webb.get(url).ensureSuccess().asJsonObject();
+            if (response.getBody().has("status") && response.getBody().getString("status").equalsIgnoreCase("UP")) {
                 System.out.println(response.toString());
                 return true;
+            } else if (response.getBody().has("status") && response.getBody().getString("status").equalsIgnoreCase("DOWN")) { // JUST IN CASE...
+                System.out.println(response.toString());
+                return false;
             }
         } catch (JSONException e) {
             System.out.println("Response not readable");
         } catch (WebbException e) {
-            System.out.println(e.getCause().getMessage());
+            if (e.getResponse() != null && e.getResponse().getErrorBody() != null) { // Usual case
+                System.out.print(e.getResponse().getErrorBody());
+            } else {
+                System.out.println("Error: [" + e.getMessage() + "]");
+            }
         }
         return false;
     }
